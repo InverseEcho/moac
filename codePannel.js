a5bc83148f527aa4fa5f39c3d9313851f099f78f
InverseEcho <36345375+InverseEcho@users.noreply.github.com>
2026-06-04 17:19:44 -0400

    Create codePannel.js

---
+++ codePannel.js
@@ -0,0 +1,21 @@
+generateImage = {import:text-to-image-plugin}
+generateText = {import:ai-text-plugin}
+superFetch = {import:super-fetch-plugin}
+fetcher = {import:yv49wuj5d6}
+
+$meta
+  title = MOAC
+  description = MOAC - AI assistant with text, image, and coding capabilities.
+  header
+    background = light-dark(grey, #101010) // background
+    mode = minimal 
+
+// Currently unfinished: 
+// Start working on keyboard shortcut fixes and updates
+// Add group chats?
+// Make persona management more intuitive
+// Add upload plugin
+
+// Q&A
+// Do the temperature and other parameters actually do anything for Perchance?
+// No, but I will be adding additional AI later that do.
